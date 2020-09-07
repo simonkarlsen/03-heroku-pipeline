fb065938418bc96301371e648b85921ba976d7a7
simonkarlsen <si-karlsen@hotmail.com>
2020-09-07 10:12:33 +0200

    Added new endpoint

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -16,6 +16,11 @@ public class DemoController {
         return "Program says: 'ping'";
     }
 
+    @GetMapping(path = "/hello")
+    public String hello() {
+        return "world!";
+    }
+
 }
 
 
